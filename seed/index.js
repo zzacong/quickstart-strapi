23e11adb3510818ddf8a66313c73632cc540808f
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-05-24 21:16:14 +1000

    upgrade packages

--- seed/index.js
+++ seed/index.js
@@ -1,31 +1,15 @@
-const path = require("path");
-const fetch = require("node-fetch");
-const { readFile } = require("fs/promises");
+const axios = require("axios");
 
 const STRAPI_SERVER = "http://localhost:1337";
 
 async function main() {
   const endpoint = "animals";
-
-  const csv = await readFile(path.join(__dirname, "seed.csv"), "utf-8");
-  const rows = csv.split("\r\n").map((row) => row.split(","));
-  // console.log(JSON.stringify(rows));
-
-  const promises = rows.map((row) =>
-    fetch(`${STRAPI_SERVER}/api/${endpoint}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        data: { name: row[0], species: row[1], birthdate: row[2] },
-      }),
-    })
-  );
+  const url = new URL(endpoint, STRAPI_SERVER);
+  const promises = rows.map((row) => axios.post(url.toString(), { data }));
 
   await Promise.all(promises);
   console.log("SUCCESS");
   process.exit(0);
 }
 
-main();
+main().catch(console.error);
